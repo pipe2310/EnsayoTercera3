0046dfff48f0b5a1cd5f6e56c0b0c8e8d95be715
pipe2310 <pipe.2310@hotmail.com>
2019-04-09 22:20:06 -0400

    tercera entrega

--- src/helpers.js
+++ src/helpers.js
@@ -3,53 +3,50 @@ const fs = require ('fs');
 const Curso = require ('../models/curso')
 const Aspirante = require ('../models/aspirante')
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 hbs.registerHelper('mostrarr',(listado)=>{
 let texto=`
 <table class='table table-striped table-hover'>
-<thead class='thead-dark'>
-<th>Identificador del Curso</th>
-<th>Nombre</th>
-<th>Descripcion</th>
-<th>Valor</th>
-<th>Modalidad</th>
-<th>Intensidad</th>
-<th>Estado</th>
-</thead>
-<tbody>`;
+    <thead class='thead-dark'>
+      <th>Identificador del Curso</th>
+      <th>Nombre</th>
+      <th>Descripcion</th>
+      <th>Valor</th>
+      <th>Modalidad</th>
+      <th>Intensidad</th>
+      <th>Estado</th>
+    </thead>
+  <tbody>`;
 if(listado){
 listado.forEach(curso=>{
 	texto=texto+
-	`<tr>
-		<td>${curso.identificador}</td>
-		<td>${curso.nombre}</td>
-		<td>${curso.descripcion}</td>
-		<td>${curso.valor}</td>
-		<td>${curso.modalidad}</td>
-		<td>${curso.intensidad}</td>
-		<td>${curso.estado}</td>
-	</tr>`;
+  	`<tr>
+  		<td>${curso.identificador}</td>
+  		<td>${curso.nombre}</td>
+  		<td>${curso.descripcion}</td>
+  		<td>${curso.valor}</td>
+  		<td>${curso.modalidad}</td>
+  		<td>${curso.intensidad}</td>
+  		<td>${curso.estado}</td>
+  	</tr>`;
 })
 }
-texto=texto+'</tbody></table>';
+texto=texto+`
+  </tbody>  
+</table>`;
 return texto;
 })
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-///////////////////////////////////////////MOSTRAR NOMBRE DE LOS CURSOS DISPONIBLES//////////////////////////////////////////////////////////////
+///////////////////////////////////////////MOSTRAR NOMBRE DE LOS CURSOS//////////////////////////////////////////////////////////////
 hbs.registerHelper('mostrarcursosnombre',(listado)=>{
 let string ;
 let out='<option value="">-</option> ';
-console.log(listado)
 
-	listado.forEach(curso=>{
-
-  out= out+` 
-   <option value="${curso.identificador}" >${curso.nombre}</option> 
-    `
-
-	})
+  listado.forEach(curso=>{
+    out= out+`<option value="${curso.identificador}" >${curso.nombre}</option>`
+  })
 
   return new hbs.SafeString(
     out 
@@ -57,19 +54,16 @@ console.log(listado)
 
 })
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ///////////////////////////////////////////MOSTRAR NOMBRE DE LOS CURSOS DISPONIBLES//////////////////////////////////////////////////////////////
 hbs.registerHelper('mostrarcursosnombre2',(listado)=>{
 let string ;
 let out='<option value="">-</option> ';
 
-
- 
-	listado.forEach(curso=>{
-if(curso.estado=="Disponible"){
-  out= out+` 
-   <option value="${curso.identificador}" >${curso.nombre}</option> 
-    `
-}
+  listado.forEach(curso=>{
+    if(curso.estado=="Disponible"){
+      out= out+`<option value="${curso.identificador}" >${curso.nombre}</option>`
+    }
 	})
 
   return new hbs.SafeString(
@@ -82,46 +76,44 @@ if(curso.estado=="Disponible"){
 ///////////////////////////////////////////MOSTRAR LOS CURSOS DISPONIBLES VER MAS2//////////////////////////////////////////////////////////////
 hbs.registerHelper('mostrarcursosdisponiblesvermas2',(listado)=>{
 let string ;
-var out='<div class="accordion" id="accordionExample"> <div class="row">';
+var out=`<div class="accordion" id="accordionExample">
+ <div class="row">`;
 var no='No se encontro un curso con el identificador ingresado';
 var sw=false;
-		i=1;
+i=1;
 	listado.forEach(curso=>{
-if(curso.estado=='Disponible' ){
+    if(curso.estado=='Disponible' ){
+        out = out +
+    `
+    <div class=".cols-sm-12 .cols-md-4 .cols-lg-12" >
+      <div class="card" >
+        <div class="card-header" id="heading${i}" >
+          <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+              Identificador del curso: ${curso.identificador}<br>
+              Nombre: ${curso.nombre}<br>
+              Valor: ${curso.valor}<br><br>
+              Ver Mas
+            </button>
+          </h2>
+        </div>
 
-    out = out +
-   `
-<div class=".cols-sm-12 .cols-md-4 .cols-lg-12" >
-  <div class="card" >
-    <div class="card-header" id="heading${i}" >
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-          Identificador del curso: ${curso.identificador}<br>
-          Nombre: ${curso.nombre}<br>
-          Valor: ${curso.valor}<br><br>
-          Ver Mas
-        </button>
-      </h2>
-    </div>
+        <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+          <div class="card-body">
+            Descripcion: ${curso.descripcion}<br>
+            Modalidad: ${curso.modalidad}<br>
+            Intensidad: ${curso.intensidad}
+          </div>
+        </div>
+     </div>
+    </div>`
+    }
+    i=i+1;
+  })
 
-    <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
-      <div class="card-body">
-        Descripcion: ${curso.descripcion}<br>
-        Modalidad: ${curso.modalidad}<br>
-        Intensidad: ${curso.intensidad}
-      </div>
-    </div>
- </div>
-</div>
-
- 
-   `
-
-}
-		//string =string+' '+curso.id;
-i=i+1;
-	})
-out=out+'</div> </div>  ';
+out=out+`
+  </div> 
+</div>`;
   return new hbs.SafeString(
     out 
   );
@@ -132,15 +124,16 @@ out=out+'</div> </div>  ';
 ///////////////////////////////////////////MOSTRAR LAS MATRICULAS//////////////////////////////////////////////////////////////
 hbs.registerHelper('mostrarmatcursos2',(listado,listadoo,listadooo)=>{
 let string ;
-var out = '<div class="accordion" id="accordionExample"> <div class="row">';
+var out = `<div class="accordion" id="accordionExample"> 
+<div class="row">`;
 i=1;
 let sw;
 
 listado.forEach(cur=>{
 sw=false;
-out=out+   `
+out=out+`
 <div class=".cols-sm-12 .cols-md-4 .cols-lg-12" >
-   <div class="card" >
+  <div class="card" >
     <div class="card-header" id="heading${i}" >
       <h2 class="mb-0">
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
@@ -151,48 +144,52 @@ out=out+   `
         </button>
       </h2>
     </div>
-     <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
-      <div class="card-body"> 
-      <table>
-<form action="/calculos6" method="post">
+  <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+<div class="card-body"> 
+  <table>
+  <form action="/calculos6" method="post">
       `
 listadoo.forEach(mat=>{
 encontradoo =listadooo.find(id=>id.identificador==mat.idaspirante)
-console.log("hola soy el encontrado")
-console.log(listadoo)
-if(sw==false && cur.identificador==mat.idcurso){
-	
-    out=out+ ` <thead>
-      <tr>
-      	<td>Documento</td>
-      	<td>Nombre</td>
-      	<td>Correo</td>
-      	<td>Telefono</td>
-      </tr>
+  if(sw==false && cur.identificador==mat.idcurso){
+      out=out+ ` 
+      <thead>
+        <tr>
+        	<td>Documento</td>
+        	<td>Nombre</td>
+        	<td>Correo</td>
+        	<td>Telefono</td>
+        </tr>
       </thead>`
-      sw=true;
-}
-if(cur.identificador==mat.idcurso){
-    out=out+`<tr>
-    <td>${encontradoo.identificador}</td>
-    	<td>${encontradoo.nombre}</td>
-    	<td>${encontradoo.correo}</td>
-    	<td>${encontradoo.telefono}</td>
-    	<input type="hidden" name="identificador" value="${encontradoo.identificador}">
-    	<input type="hidden" name="identificadorcurso" value="${cur.identificador}">
-    	<td><button name="matricula" value="${cur.identificador+''+encontradoo.identificador}">Eliminar</button></td>
-    	</tr>
-   `
-}
-
-	})
-out=out+ `</table></form></div></div></div></div>
+        sw=true;
+  }
+  if(cur.identificador==mat.idcurso){
+      out=out+`
+        <tr>
+          <td>${encontradoo.identificador}</td>
+        	<td>${encontradoo.nombre}</td>
+        	<td>${encontradoo.correo}</td>
+        	<td>${encontradoo.telefono}</td>
+        	<input type="hidden" name="identificador" value="${encontradoo.identificador}">
+        	<input type="hidden" name="identificadorcurso" value="${cur.identificador}">
+        	<td><button name="matricula" value="${cur.identificador+''+encontradoo.identificador}">Eliminar</button></td>
+      	</tr>`
+  }
+})
+out=out+ `
+  </table>
+  </form>
+</div>
+</div>
+</div>
+</div>
    		`
 i=i+1;
 	})
 
 	out=out+  `
-   		</div></div>`
+   </div>
+</div>`
 
   return new hbs.SafeString(
     out 
@@ -217,49 +214,52 @@ out=out+   `
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
           Identificador del curso: ${cur.identificador}<br>
           Nombre: ${cur.nombre}<br>
-    
-
         </button>
       </h2>
     </div>
      <div id="collapse${i}" class="collapse show" aria-labelledby="heading${i}" data-parent="#accordionExample">
       <div class="card-body"> 
       <table>
-
       `
 matriculaa.forEach(mat=>{
-
 encontradoo =listadoo.find(id=>id.identificador==mat.idaspirante)
 if(sw==false && cur.identificador==mat.idcurso){
 	
-    out=out+ ` <thead>
+    out=out+ ` 
+    <thead>
       <tr>
       	<td>Documento</td>
       	<td>Nombre</td>
       	<td>Correo</td>
       	<td>Telefono</td>
       </tr>
-      </thead>`
+    </thead>`
       sw=true;
 }
 if(cur.identificador==mat.idcurso){
-    out=out+`<tr>
-    <td>${encontradoo.identificador}</td>
+    out=out+`
+    <tr>
+      <td>${encontradoo.identificador}</td>
     	<td>${encontradoo.nombre}</td>
     	<td>${encontradoo.correo}</td>
     	<td>${encontradoo.telefono}</td>
-    	</tr>
+    </tr>
    `
 }
 
 	})
-out=out+ `</table></div></div></div></div>
-   		`
+out=out+ `
+</table>
+</div>
+</div>
+</div>
+</div>`
 i=i+1;
 }
 	})
-	out=out+  `
-   		</div></div>`
+	out=out+`
+</div>
+</div>`
 
   return new hbs.SafeString(
     out 
